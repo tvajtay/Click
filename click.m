d9da76bd73abb943899d8f9a6236edc754120fa4
Tom <vajtayt@gmail.com>
2016-06-29 18:19:32 -0400

    Removed batch.py from classify and reclassify

--- click.m
+++ click.m
@@ -6,31 +6,54 @@ function [ ] = click(face_hint,whiskers)
 %hint' as a string and "whiskers" as an integer
 %  
 
-dos('python python/batch.py data -e trace -f *.tif')
+dos('python python/batch.py data -e trace -f *.mp4') 
 
 %dos('python python/batch.py data -e whisker_convert --args="whisk1" -f *.whiskers')
+%command to format all whiskers files to whisk1 format turn on if recieving
+%format error from the measure section below.
 
 cd data
 files = dir('*.whiskers');
 cd ..
-W = size(files);
-W = W(1);
-for n = 1:W
+M = size(files);
+M = M(1);
+for n = 1:M
     file = files(n);
     measures = file.name;
     measures = measures(1:end-8);
     measures = [measures 'measurements'];
     fprintf(1,'Converting %s\n',file.name)
-    stringm = sprintf('measure --face %s data/%s data/%s ', face_hint, file.name, measures)
+    stringm = sprintf('measure --face %s data/%s data/%s ', face_hint, file.name, measures);
     dos(stringm)
 end
-fprintf('Conversion complete')
+fprintf('Measurement complete')
 
-stringc = sprintf('python python/batch.py data -e classify --args="%s --px2mm 0.04 -n %1.0f" -f *.measurements',face_hint,whiskers);
-dos(stringc);
+cd data
+files = dir('*.measurements');
+cd ..
+M = size(files);
+M = M(1);
+for n = 1:M
+    file = files(n);
+    fprintf(1,'Converting %s\n',file.name)
+    stringc = sprintf('classify data/%s data/%s %s --px2mm 0.04 -n %1.0f ', file.name, file.name, face_hint, whiskers);
+    dos(stringc)
+end
+fprintf('Classification complete')
 
-stringrc = sprintf('python python/batch.py data -e reclassify -f *.measurements');
-dos(stringrc);
+cd data
+files1 = dir('*.measurements');
+cd ..
+S = size(files1);
+S = S(1);
+for n = 1:S
+    file = files1(n);
+    fprintf(1,'Converting %s\n',file.name)
+    stringc = sprintf('reclassify -n %1.0f data/%s data/%s ', whiskers, file.name, file.name);
+    dos(stringc)
+end
+fprintf('Reclassification complete')
+
 
 cd data
 measurements_files = dir('*.measurements');
@@ -43,9 +66,8 @@ for i = 1:d
         B = ['data/' file.name];
         table = LoadMeasurements(B);
         cd analyzed
-        file.name = file.name(1:end-12);
-        file.name = [file.name 'mat'];
-        name = file.name;
+        name = file.name(1:end-12);
+        name = [name 'mat'];
         save(name, 'table');
         cd ..
 end
