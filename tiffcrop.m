ba7197b08f52fd1776efba6c22eb3f690f6223f0
Tom Vajtay <vajtayt@gmail.com>
2017-03-02 14:49:57 -0500

    Add files via upload

---
+++ tiffcrop.m
@@ -0,0 +1,65 @@
+function [] = tiffcrop( start_side, start_dir, coordinates )
+%TIFFCROP Crops all tiff files 
+%   Detailed explanation goes here
+tstart = tic;
+working_directory = cd;
+addpath(cd)
+addpath matlab
+addpath(start_dir);
+
+function [fold_detect,file_detect] = detector(path)
+        cd(path)
+        b = dir();
+        files = dir('*.tif');
+        isub = [b(:).isdir];
+        nameFolds = {b(isub).name}';
+        nameFolds(ismember(nameFolds,{'.','..'})) = [];
+        fold_detect = size(nameFolds, 1);
+        file_detect = size(files, 1);
+end
+
+function [] = cropp(side, direc, coord, filenum)
+        fprintf('Tif files located, beginning to correct.\n');
+        cd(direc);
+        images = dir('*.tif');
+        for t = 1:filnum
+            curr_file = images(t).name;
+            fprintf('Loading image data for %s.\n',curr_file);
+            info = imfinfo(curr_file); %creates structure for every frame in tif stack
+            elements = numel(info); %determine the number of frames 
+            
+        end
+
+end
+
+
+
+coord_num = 0;
+[fold,fil] = detector(start_dir);
+if fil > 0
+    coord_num = coord_num + 1;
+    coordinate = coordinates(coord_num);
+    cropp(start_side, start_dir, coordinate, fil);
+elseif fil == 0
+    fprintf('No tif files in the start directory\n');
+end
+
+if fold > 0
+    target = [start_directory '\**\*.'];
+    fprintf('Scanning all subdirectories from starting directory\n');
+    D = rdir(target);             %// List of all sub-directories
+    for k = 1:length(D)
+        currpath = D(k).name;
+        [~,fil] = detector(currpath);
+        fprintf('Checking %s for tif files\n', currpath);
+        if fil > 0
+            coord_num = coord_num + 1;
+            coordinate = coordinates(coord_num);
+            cropp(start_side, currpath, coordinate, fil);
+        end
+    end
+end
+        
+
+end
+
