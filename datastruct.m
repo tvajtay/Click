6e18baa89901b78b1ba652afd3ec1562ed33478e
Tom Vajtay <vajtayt@gmail.com>
2017-05-15 14:39:54 -0400

    Add files via upload

---
+++ datastruct.m
@@ -0,0 +1,13 @@
+function [ D ] = datastruct( path )
+%DATASTRUCT Function to organize optogenetic whisker data
+%   The testing paradigm of the summer 2016 optogenetic testing and beyond
+% uses a unique sequence of durations. This function aims to organize all
+% the individual data into a 3D struct with averages and SEM included. The
+% input arguement is only a string of the path to the main parent directory
+% of the experiment.
+
+
+
+
+end
+
